e15cd27f6571630f1f7d7b4bf218805fea3cf2e7
K M H Mubin <kmhmubin@gmail.com>
2022-03-04 20:01:36 +0600

    frontend #5, #6: add custom css, header and footer

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,44 @@
-    <!-- Boostrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
+<!--footer starts-->
+<footer class="footer">
+    <div class="footer-content">
+        <div class="footer-section about">
+            <h1 class="logo-text"><span>NSUer's</span> Blogs</h1>
+            <p>This website is only for NSU student. On this platfrom students a free to
+                share their various ideas and post their blogs. </p>
+            <div class="contact">
+                <span>North South University</span><br>
+                <span><i class="fa-solid fa-location-dot"></i> Bashundhara, Dhaka-1229, Bangladesh</span><br>
+                <span><i class="fas fa-phone"></i> +880-2-55668200</span><br>
+                <span><i class="fas fa-envelope"></i> registrar@northsouth.edu</span>
+            </div>
+        </div><br>
+        <div class="footer-section links">
+            <h2>Quick Links</h2><br>
+            <a href="#"><i class="fa-brands fa-facebook"></i> Facebook</a><br><br>
+            <a href="#"><i class="fa-brands fa-twitter"></i> Twitter</a><br><br>
+            <a href="#"><i class="fa-brands fa-instagram-square"></i> Instagram</a><br><br>
+            <a href="#"><i class="fab fa-youtube"></i> Youtube</a>
+        </div>
+        <div class="footer-section contact-form">
+            <h2>Contact Us</h2><br>
+            <form action="home-page.html" method="post">
+                <input type="email" name="email" class="text-input contact-input" placeholder="Your email Address...."><br>
+                <textarea name="message" class="text-input contact-input" placeholder="Your message...."></textarea><br>
+                <button type="submit" class="btn btn-big">
+                    <i class="fas fa-envelope"></i> Send
+                </button>
+            </form>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        &copy;All Rights Reserved. Designed By <a href="#"><b>CSE482 Project Team</b></a>
+    </div>
+</footer>
 
-    </html>
+<!--footer ends-->
+
+<!--custom scrip-->
+<script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
--- include/header.php
+++ include/header.php
@@ -8,13 +8,13 @@
     <!-- favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
     <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
-    <title>Daily NSU Blog</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <!-- FontAwesome Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="assets/css/style.css" />
+    <title>NSU Blogs</title>
 </head>
 
 <body>
