e81ded50cb72e2a57983e77721041e195758cb72
K M H Mubin <kmhmubin@gmail.com>
2022-03-05 14:10:24 +0600

    frontend #5: complete the discussion page

--- discussion.php
+++ discussion.php
@@ -2,252 +2,133 @@
 include('include/header.php');
 ?>
 
-<main class="container">
-    <!-- Discussion Section -->
-    <div>
-
-        <div class="row mt-2 align-items-center">
-            <div class="col-sm-9">
-                <h1>General Discussion</h1>
-                <p>Ask a question, post a suggestion, start a discussion.</p>
-            </div>
-            <div class="col-sm-3">
-                <a href="#" class="btn btn-primary">Create New Thered</a>
+<main class="container mtb-2">
+    <section class="discussion-header">
+        <div class="discussion-title">
+            <h1>General Discussion</h1>
+            <p>Ask a question, post a suggestion, start a discussion.</p>
+        </div>
+        <div class="discussion-create">
+            <div class="mt-2">
+                <a href="#" class="btn">Create New Thered</a>
             </div>
         </div>
-        <hr class="bg-secondary border-2 border-top border-secondary">
+    </section>
+    <div>
+        <hr />
     </div>
 
-    <!-- Discussion thered sections -->
+    <!-- Discussion Table data -->
+
     <section>
-        <div>
-            <table class="table">
+        <div class="mtb-2">
+            <table>
                 <thead>
                     <tr>
                         <th>Topic</th>
                         <th>Category</th>
+                        <th>Views</th>
                         <th>Likes</th>
                         <th>Replies</th>
-                        <th>Views</th>
                         <th>Activity</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>
+                        <td data-label="Topic">
                             <a href="#">
-                                <h6>How to make a website</h6>
+                                <h4>How to make a website</h4>
                             </a>
                         </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
+                        <td data-label="Category">
+                            <a href="#" class="text-center" role="button">
                                 Web Development
                             </a>
                         </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
+                        <td data-label="Views">12 k</td>
+                        <td data-label="Like">314</td>
+                        <td data-label="Relpy">33</td>
+                        <td data-label="Activity">1 hr</td>
                     </tr>
                     <tr>
-                        <td>
+                        <td data-label="Topic">
                             <a href="#">
-                                <h6>How to make a website</h6>
+                                <h4>How to make a website</h4>
                             </a>
                         </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
+                        <td data-label="Category">
+                            <a href="#" class="text-center" role="button">
                                 Web Development
                             </a>
                         </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
+                        <td data-label="Views">12 k</td>
+                        <td data-label="Like">314</td>
+                        <td data-label="Relpy">33</td>
+                        <td data-label="Activity">1 hr</td>
                     </tr>
                     <tr>
-                        <td>
+                        <td data-label="Topic">
                             <a href="#">
-                                <h6>How to make a website</h6>
+                                <h4>How to make a website</h4>
                             </a>
                         </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
+                        <td data-label="Category">
+                            <a href="#" class="text-center" role="button">
                                 Web Development
                             </a>
                         </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
+                        <td data-label="Views">12 k</td>
+                        <td data-label="Like">314</td>
+                        <td data-label="Relpy">33</td>
+                        <td data-label="Activity">1 hr</td>
                     </tr>
                     <tr>
-                        <td>
+                        <td data-label="Topic">
                             <a href="#">
-                                <h6>How to make a website</h6>
+                                <h4>How to make a website</h4>
                             </a>
                         </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
+                        <td data-label="Category">
+                            <a href="#" class="text-center" role="button">
                                 Web Development
                             </a>
                         </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
+                        <td data-label="Views">12 k</td>
+                        <td data-label="Like">314</td>
+                        <td data-label="Relpy">33</td>
+                        <td data-label="Activity">1 hr</td>
                     </tr>
                     <tr>
-                        <td>
+                        <td data-label="Topic">
                             <a href="#">
-                                <h6>How to make a website</h6>
+                                <h4>How to make a website</h4>
                             </a>
                         </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
+                        <td data-label="Category">
+                            <a href="#" class="text-center" role="button">
                                 Web Development
                             </a>
                         </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
+                        <td data-label="Views">12 k</td>
+                        <td data-label="Like">314</td>
+                        <td data-label="Relpy">33</td>
+                        <td data-label="Activity">1 hr</td>
                     </tr>
                     <tr>
-                        <td>
+                        <td data-label="Topic">
                             <a href="#">
-                                <h6>How to make a website</h6>
+                                <h4>How to make a website</h4>
                             </a>
                         </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
+                        <td data-label="Category">
+                            <a href="#" class="text-center" role="button">
                                 Web Development
                             </a>
                         </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <h6>How to make a website</h6>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
-                                Web Development
-                            </a>
-                        </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <h6>How to make a website</h6>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
-                                Web Development
-                            </a>
-                        </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <h6>How to make a website</h6>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
-                                Web Development
-                            </a>
-                        </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <h6>How to make a website</h6>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
-                                Web Development
-                            </a>
-                        </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <h6>How to make a website</h6>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
-                                Web Development
-                            </a>
-                        </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <a href="#">
-                                <h6>How to make a website</h6>
-                            </a>
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-sm btn-warning text-center" role="button">
-                                Web Development
-                            </a>
-                        </td>
-                        <td>
-                            314
-                        </td>
-                        <td>33</td>
-                        <td>12 k</td>
-                        <td>1 hr</td>
+                        <td data-label="Views">12 k</td>
+                        <td data-label="Like">314</td>
+                        <td data-label="Relpy">33</td>
+                        <td data-label="Activity">1 hr</td>
                     </tr>
                 </tbody>
             </table>
