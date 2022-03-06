0695c954740440c26e1ed76940de2f99dac6b521
K M H Mubin <kmhmubin@gmail.com>
2022-03-07 01:35:45 +0600

    frontend #3: add sign up page

--- signup.php
+++ signup.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Front awesome link-->
+    <script src="https://kit.fontawesome.com/4e17d14c86.js" crossorigin="anonymous"></script>
     <!--Google fonts link-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
     <!--Custom CSS Styling-->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Sign-up form</title>
 </head>
 
@@ -55,8 +57,12 @@
             <p>Or <a href="login.html">Log in</a> </p>
         </form>
     </div>
-    <!--sign up form end-->
+    <!--sign up form starts-->
+    <!--Jquary-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    <!--custom scrip-->
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
