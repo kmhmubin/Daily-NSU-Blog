1cff5b0e4f4887c7246f8e6838f1ab0adb50591b
K M H Mubin <kmhmubin@gmail.com>
2022-03-07 01:28:17 +0600

    frontend #4: add single article page with comment section

---
+++ article.php
@@ -0,0 +1,83 @@
+<?php
+include('include/header.php');
+?>
+
+<main class="container mtb-2">
+
+    <!--page wapper starts-->
+    <div class="page-wrapper">
+
+
+        <!--content section starts-->
+        <div class="content clearfix">
+            <!--main-container-->
+            <div class="main-content-wrapper">
+                <div class="main-content single">
+                    <h2 class="post-title">25th Convocation Extended date of degree application</h2>
+                    <div class="post-content">
+                        <p>1. Students having completed all requires of their respective degree in or before the Fall-2021
+                            semester and yet to submit degree application gor graduation through online to join the next
+                            convocation are requested to submit their degree application with in 15 March 2022.
+                        </p>
+                        <p>1. Students having completed all requires of their respective degree in or before the Fall-2021
+                            semester and yet to submit degree application gor graduation through online to join the next
+                            convocation are requested to submit their degree application with in 15 March 2022.
+                        </p>
+                        <p>1. Students having completed all requires of their respective degree in or before the Fall-2021
+                            semester and yet to submit degree application gor graduation through online to join the next
+                            convocation are requested to submit their degree application with in 15 March 2022.
+                        </p>
+                        <p>1. Students having completed all requires of their respective degree in or before the Fall-2021
+                            semester and yet to submit degree application gor graduation through online to join the next
+                            convocation are requested to submit their degree application with in 15 March 2022.
+                        </p>
+                    </div>
+
+                    <!-- Comments section -->
+                    <section>
+                        <div>
+                            <h1>comments <span>( 3 )</span></h1>
+                        </div>
+                        <?php
+                        include('comments.php');
+                        ?>
+                    </section>
+
+                </div>
+            </div>
+            <!--main-container-->
+
+            <!--sidebar section section starts -->
+            <div class="sidebar single">
+                <div class="section topics">
+                    <h2 class="section-title">Topics</h2>
+                    <ul>
+                        <li><a href="">Academic</a></li>
+                        <li><a href="">Examinations</a></li>
+                        <li><a href="">Noties</a></li>
+                        <li><a href="">Rescarch</a></li>
+                        <li><a href="">Campus Job</a></li>
+                        <li><a href="">Motivation</a></li>
+                        <li><a href="">Life lessom</a></li>
+                    </ul>
+                </div>
+
+                <div class="comment">
+                    <textarea name="message" class="text-input contact-input" placeholder="Add your comment....."></textarea><br>
+                    <button type="submit" class="btn btn-big contact-btn">
+                        <i class="fas fa-envelope"></i> Submit
+                    </button>
+                </div>
+            </div>
+            <!--sidebar section section ends -->
+        </div>
+        <!--content section Ends-->
+    </div>
+    <!--page wapper ends-->
+
+
+</main>
+
+<?php
+include('include/footer.php');
+?>
