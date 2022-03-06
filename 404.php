e3f682cea1f3b46462321a07748209a599c5834e
K M H Mubin <kmhmubin@gmail.com>
2022-03-07 02:25:22 +0600

    Frontend #14: add 404 page

---
+++ 404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--Front awesome link-->
+    <script src="https://kit.fontawesome.com/4e17d14c86.js" crossorigin="anonymous"></script>
+    <!--Google fonts link-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong" />
+    <!--Custom CSS Styling-->
+    <link rel="stylesheet" href="css/style.css" />
+    <title>NSU Blogs</title>
+</head>
+
+<body>
+    <div class="error-page">
+        <div class="content">
+            <h2>404</h2>
+            <h4>Sorry! Page is not found</h4>
+            <p>The requested page was not found on this server.</p>
+            <div class="btns">
+                <a href="#">Return Home</a>
+                <a href="#">Report Problem</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
