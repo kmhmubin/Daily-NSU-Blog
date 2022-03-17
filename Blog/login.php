dee674d493ec881ce3f3d0f56c8530c1f29a35fe
K M H Mubin <kmhmubin@gmail.com>
2022-03-17 22:04:53 +0600

    auth #16: connect login form with backend and user validation

--- Blog/login.php
+++ Blog/login.php
@@ -1,4 +1,8 @@
-<?php include('path.php'); ?>
+<?php
+include('path.php');
+include(ROOT_PATH . "/app/controllers/users.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,25 +30,26 @@
     <div class="signup-content">
         <form action="signup.php">
             <h2 class="form-title">Log in</h2>
+
+            <?php include(ROOT_PATH . "/app/helpers/formErrors.php") ?>
             <div>
                 <label>Username</label>
-                <input type="text" name="username" class="text-input">
+                <input type="text" name="username" value=<?php echo $username; ?> class="text-input">
             </div>
 
             <div>
                 <label>Password</label>
-                <input type="password" name="password" class="text-input">
+                <input type="password" name="password" value=<?php echo $password; ?> class="text-input">
             </div>
 
             <div>
                 <button type="submit" name="login-btn" class="btn btn-big">Log in</button>
             </div>
-            <p>Or <a href="<?php echo Base_URL . 'signup.php' ?>">Register</a> </p>
+            <p>Or <a href="<?php echo Base_URL . '/signup.php' ?>">Register</a> </p>
         </form>
     </div>
     <!--sign up form starts-->
-    <!--Jquary-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 
     <!--custom scrip-->
     <script src="assets/js/Validation.js"></script>
