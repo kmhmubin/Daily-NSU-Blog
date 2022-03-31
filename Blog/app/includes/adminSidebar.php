323300379d79e6b2160bc972c13cdddd179f033e
K M H Mubin <kmhmubin@gmail.com>
2022-03-31 21:58:32 +0600

    enhance #17: add navlinks to the admin sidebar

--- Blog/app/includes/adminSidebar.php
+++ Blog/app/includes/adminSidebar.php
@@ -2,21 +2,42 @@
 <div class="left-sidebar">
     <ul>
         <li>
-            <a href="<?php echo Base_URL . 'admin/posts/index.php'; ?>">
-                Manage Posts
-            </a>
-        </li>
-        <li>
             <a href="<?php echo Base_URL . 'admin/users/index.php'; ?>">
                 Manage Users
             </a>
         </li>
         <li>
+            <a href="<?php echo Base_URL . 'admin/posts/index.php'; ?>">
+                Blog Posts
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo Base_URL . 'admin/comments/index.php'; ?>">
+                Post Comments
+            </a>
+        </li>
+        <li>
             <a href="<?php echo Base_URL . 'admin/topic/index.php'; ?>">
-                Manage Topics
+                Post Topics
             </a>
         </li>
 
+        <li>
+            <a href="<?php echo Base_URL . 'admin/threads/index.php'; ?>">
+                Discussion Threads
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo Base_URL . 'admin/discussions/index.php'; ?>">
+                Threads Comments
+            </a>
+        </li>
+
+        <li>
+            <a href="<?php echo Base_URL . 'admin/threads-topic/index.php'; ?>">
+                Threads Topics
+            </a>
+        </li>
     </ul>
 </div>
 <!--left side ber ends-->
