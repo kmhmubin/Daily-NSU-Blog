e8e59f02b956ca55d2e316c684ea674a4d5b3702
K M H Mubin <kmhmubin@gmail.com>
2022-04-18 00:44:01 +0600

    remove: old codes

--- Blog/path.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("ROOT_PATH", realpath(dirname(__FILE__)));
-// universal localhost
-// define("Base_URL", "http://localhost/blog");
-// for my localhost
-define("Base_URL", "http://localhost:8080/nsuers-blog/Blog/");
